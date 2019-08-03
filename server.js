0eb6ed464da3071a317f238014cf940c69f3c6cf
Jason <KONIG@Jasons-MBP.fios-router.home>
2019-08-03 14:14:38 -0400

    Second commit

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const connectDB = require("./config/db");
+
 
 const app = express();
+
+// Todo 1: Connect to Database
+connectDB();
 
 app.get("/", (req, res) => res.send('API running'))
 
